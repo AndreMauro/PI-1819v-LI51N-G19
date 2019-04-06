870bbb5ca865a790398d3b27ee4a6cf6d6d2a3ca
André Mauro <andrexzibit@gmail.com>
2019-04-06 16:45:58 +0100

    nothing special only

---
+++ Projecto/last.fm-data.js
@@ -0,0 +1,13 @@
+
+
+const api_key = b77a32de4783768b503960440aa1740e;
+const url = 'http://ws.audioscrobbler.com/2.0/?method=';
+const method = 'artist.search';
+//http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=b77a32de4783768b503960440aa1740e&format=json
+
+//http://ws.audioscrobbler.com/2.0/?method=album.search&album=believe&api_key=b77a32de4783768b503960440aa1740e&format=json
+
+//http://ws.audioscrobbler.com/2.0/?method=album.getInfo&mbid=c559efc2-f734-41ae-93bd-2d78414e0356&api_key=b77a32de4783768b503960440aa1740e&format=json
+
+
+//
